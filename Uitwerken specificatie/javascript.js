a31e8384acc714f6722003575ed69047f0b70e0b
Teun van der Ploeg <29253@ma-web.nl>
2020-09-15 10:47:22 +0200

    text weg gehaald
    text weg gehaald

--- Uitwerken specificatie/javascript.js
+++ Uitwerken specificatie/javascript.js
@@ -1,30 +1,7 @@
-// console.log("Wat is je naam?");
-// var name = raw_input();
-
-// console.log("Wat is je klas?");
-// var klas = raw_input();
-
-// var persoon = {
-//   naam: name,
-//   klas: klas,
-// };
-
-// console.log(persoon);
-
-// var name = window.prompt("Enter your name: ");
-// var klas = window.prompt("Enter your klas: ");
-// var persoon = {
-//   naam: name,
-//   klas: klas,
-// };
-// console.log(persoon);
-// alert(
-//   "Your name is " + name + " Je klas is" + klas + " Het staat ook in de console"
-// );
-// console.log(persoon);
-
 var studenten = [];
+
 studenten = localStorage.getItem("alleStudenten");
+
 function Student(id, klas) {
   let studentNummer = id;
   let klasNaam = klas;
@@ -33,5 +10,6 @@ function Student(id, klas) {
   console.log(studenten);
   localStorage.setItem(studenten);
 }
+
 var storage = localStorage.getItem("studenten");
 console.log(storage);
