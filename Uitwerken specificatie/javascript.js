5e622005d0f4393d9c285d61762bb8053564fce2
Teun van der Ploeg <29253@ma-web.nl>
2020-09-15 10:47:01 +0200

    specificaties
    specificaties

--- Uitwerken specificatie/javascript.js
+++ Uitwerken specificatie/javascript.js
@@ -11,14 +11,27 @@
 
 // console.log(persoon);
 
-var name = window.prompt("Enter your name: ");
-var klas = window.prompt("Enter your klas: ");
-var persoon = {
-  naam: name,
-  klas: klas,
-};
-console.log(persoon);
-alert(
-  "Your name is " + name + " Je klas is" + klas + " Het staat ook in de console"
-);
-console.log(persoon);
+// var name = window.prompt("Enter your name: ");
+// var klas = window.prompt("Enter your klas: ");
+// var persoon = {
+//   naam: name,
+//   klas: klas,
+// };
+// console.log(persoon);
+// alert(
+//   "Your name is " + name + " Je klas is" + klas + " Het staat ook in de console"
+// );
+// console.log(persoon);
+
+var studenten = [];
+studenten = localStorage.getItem("alleStudenten");
+function Student(id, klas) {
+  let studentNummer = id;
+  let klasNaam = klas;
+  var student = { numer: studentNummer, klas: klasNaam };
+  studenten.push(student);
+  console.log(studenten);
+  localStorage.setItem(studenten);
+}
+var storage = localStorage.getItem("studenten");
+console.log(storage);
